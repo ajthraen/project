22ee6e5649f9a411003616f2515feb62dfc4360f
AJ <ajthraen@gmail.com>
2022-10-23 12:33:21 -0400

    firebase login2

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
 import Logo from '../assets/Library.svg'
+import { auth } from "../firebase/init"
+import { 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "firebase/auth";
 
 const Nav = () => {
+    const [user, setUser] = React.useState({});
+    const [loading, setLoading] = React.useState(true);
+  
+    React.useEffect(() => {
+      onAuthStateChanged(auth, (user) => {
+        setLoading(false);
+        console.log(user)
+        if (user) {
+          setUser(user)
+        }
+      })
+    }, []);
+    function register() {
+      createUserWithEmailAndPassword(auth, 'a@gmail.com', 'password123')
+      .then((user) => {
+        console.log(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+    function login() {
+      console.log('login')
+      signInWithEmailAndPassword(auth, 'a@gmail.com', 'password123' )
+      .then(({user}) => {
+        console.log(user)
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+    function logout() {
+      console.log('logout');
+      signOut(auth);
+      setUser({});
+    }
     return (
         <div className='container'>
             <div className="leftBox">
@@ -10,9 +54,10 @@ const Nav = () => {
                 </a>
             </div>
             <div className="rightBox">
-                <a className="navLinks" href="/">Register</a>
-                <a className="navLinks" href="">Login</a>
-                <a className="navLinks" href="/page">Home</a>
+                <button className='navLinks' onClick={register}>Register</button>
+                <button className='navLinks' onClick={login}>Login</button>
+                <button className='navLinks' onClick={logout}>Logout</button>
+                <div className='userBubble'>{loading ? 'loading...' : user.email[0].toUpperCase()}</div>
             </div>
         </div>
     );
