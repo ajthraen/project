037b07e3ebae87c93e72c86c49366dab72a0b5f6
AJ <ajthraen@gmail.com>
2022-11-01 22:21:09 -0400

    form with firebase working

--- pages/Form.jsx
+++ pages/Form.jsx
@@ -2,40 +2,33 @@ import React from 'react';
 import { useState } from 'react';
 
 export const Form = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [age, setAge] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const users = {email, password}
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${firstName}`)
-        console.log(firstName)
+        // alert(`The name you entered was: ${email}`)
+        users[email, password] = {email, password}
+        console.log(users.email)
     }
 
     return (
     <form onSubmit={onSubmitHandler}>
-        <div><label htmlFor="first_name">First Name</label></div>
+        <div><label htmlFor="first_name">Email</label></div>
         <div>        
             <input
                 type="text" 
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
             />
         </div>
-        <div><label htmlFor="last_name">Last Name</label></div>
+        <div><label htmlFor="last_name">Password</label></div>
         <div>
             <input
                 type="text" 
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-        </div>
-        <div><label htmlFor="age">Age</label></div>
-        <div>
-            <input
-                type="text" 
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
         </div>
         <input type="submit"/>
