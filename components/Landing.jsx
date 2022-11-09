e103c4bb17a5a45be1059a776f10dcf17632dc5c
AJ <ajthraen@gmail.com>
2022-11-08 21:58:01 -0500

    homepage overhaul1

---
+++ components/Landing.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import UndrawDog from '../assets/undraw_good_dog.svg'
+
+const Landing = () => {
+    return (
+        <section id='landing'>
+            <header>
+                <div className="header__container">
+                    <div className="header__description">
+                        <h1>Learn how to train your dog online!</h1>
+                        <h2>Help your dog reach their <span className='purple'>Potential</span></h2>
+                        <a href="#features">
+                            <button className='btn'>How Do I Start?</button>
+                        </a>
+                    </div>
+                    <figure className="header__img--wrapper">
+                        <img src={UndrawDog} alt="" />
+                    </figure>
+                </div>
+            </header>
+
+        </section>
+    );
+}
+
+export default Landing;
