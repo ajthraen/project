0df5e497567991fd4c2921feff30ebf72e038a3e
AJ <ajthraen@gmail.com>
2022-11-11 21:05:18 -0500

    page2

--- components/Sibebar.jsx
+++ components/Sibebar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sibebar = () => {
     return (
         <div className='sidebar'>
-            This is the sidebar
+            <Link to="/page" className="sidebar__title">Training Center</Link>
+            <div className="sidebar__section">
+                <h4>Course 1</h4>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+            </div>
+            <div className="sidebar__section">
+                <h4>Course 2</h4>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+            </div>
+            <div className="sidebar__section">
+                <h4>Course 3</h4>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+            </div>
+            <div className="sidebar__section">
+                <h4>Course 4</h4>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+                <Link to="/page" className="sidebar__link">Training Center</Link>
+            </div>
         </div>
     );
 }
