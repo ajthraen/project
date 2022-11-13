1133e595f21435c3c6d3d26588120d9637cc13fe
AJ <ajthraen@gmail.com>
2022-11-13 12:46:12 -0500

    bootstrap accordian

--- components/Discounted.jsx
+++ components/Discounted.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 // import { books } from '../data'
 // import Book from './ui/Book';
 
 
 const Discounted = () => {
     return (
-        <section id="discount">
-            <div className="container">
-                <div className="row">
-                    <div className="section__title">
-                        <h2>Discounted <span className="purple">Books</span></h2>
-                    </div>
-                    <div className="books">
- 
-                    </div>
-                </div>
+      <section id="discount">
+        <div className="container">
+          <div className="row m-0">
+            <div className="section__title">
+              <h2>
+                Discounted <span className="purple">Books</span>
+              </h2>
             </div>
-        </section>
+            <div>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Accordion Item #1</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Accordion Item #2</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          </div>
+        </div>
+      </section>
     );
 }
 
--- components/Explore.jsx
+++ components/Explore.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Explore = () => {
     return (
         <section id="explore">
-            <div className="container">
-                <div className="row row__column">
-                    <h2>
-                        Start your Training <span className="purple">Journey</span>
-                    </h2>
-                    <Link to="/userpage">
-                        <button className="btn">Sign Up!</button>
-                    </Link>
+            <div className="container p-4">
+                <div className='d-flex flex-direction-column align-item-center'>
+                    <div>
+                        <h2>Start your Training <span className="purple">Journey</span></h2>
+                    </div>
+                    <div className='m-3'>
+                        <Link to="/userpage"><button className="btn2">Sign Up!</button></Link>
+                    </div>
                 </div>
             </div>
         </section>
