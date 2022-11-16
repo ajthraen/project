fbde239b2fbb61be2557fb896b24e679f295438f
AJ <ajthraen@gmail.com>
2022-11-15 20:41:27 -0500

    bootstrap sidebar

--- src/components/Sidetest.jsx
+++ src/components/Sidetest.jsx
@@ -5,11 +5,13 @@ import Collapse from 'react-bootstrap/Collapse';
 
 const Sidetest = () => {
     const [open, setOpen] = useState(false);
+    const [open1, setOpen1] = useState(false);
+    const [open2, setOpen2] = useState(false);
 
     return (
         <div className='sidebar'>
             <Link to="/page" className="sidebar__title">Training Center</Link>
-            <div>
+            <div className='p-3'>
                 <Button
                     onClick={() => setOpen(!open)}
                     aria-controls="example-collapse-text"
@@ -17,7 +19,42 @@ const Sidetest = () => {
                 >
                     \/
                 </Button>
+                Course 1
                 <Collapse in={open}>
+                    <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </Collapse>
+            </div>
+            <div className='p-3'>
+                <Button
+                    onClick={() => setOpen1(!open1)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open1}
+                >
+                    \/
+                </Button>
+                Course 2
+                <Collapse in={open1}>
+                    <div id="example-collapse-text">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                    labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </Collapse>
+            </div>
+            <div className='p-3'>
+                <Button
+                    onClick={() => setOpen2(!open2)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open2}
+                >
+                    \/
+                </Button>
+                Course 3
+                <Collapse in={open2}>
                     <div id="example-collapse-text">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
                     terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
